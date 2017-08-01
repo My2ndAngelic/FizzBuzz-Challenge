f99cc41d77c200674db099762360f6d423fb9aaa
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2017-08-01 19:36:55 +0200

    Change things
    Remove something and add again

---
+++ Pascal/L1-1.pas
@@ -0,0 +1,32 @@
+uses crt, sysutils;
+
+function Check3Div:boolean;
+begin
+  if i mod 3 = 0
+    Check3Div:= TRUE
+  else Check3Div:= FALSE;
+end;
+
+function Check5Div:boolean;
+begin
+  if i mod 5 = 0
+    Check5Div:= TRUE
+  else Check5Div:= FALSE;
+end;
+
+begin
+  o:='';
+  write('Please enter n: ');
+  readln(n);
+  for i:= 1 to n do
+    begin
+      if Check3Div = TRUE then
+        write('Fizz');
+      else if check5Div = TRUE then
+        write('Buzz');
+      else write(i);
+      writeln;
+    end;
+  readln;
+
+end.
